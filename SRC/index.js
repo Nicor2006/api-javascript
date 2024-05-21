8728a6b58c299fcbce81eafefcaa9e2da005e2fc
NicolasRivera <nicolasrpv02@gmail.com>
2024-05-21 18:59:05 -0500

    second commit

--- SRC/index.js
+++ SRC/index.js
@@ -2,7 +2,7 @@ const app = require ("./app");
 
 const PORT = process.env.PORT;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 
   console.log("Servidor ejecutandose en http://localhost:3000/");
 
